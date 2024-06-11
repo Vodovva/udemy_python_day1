14f3e9867c80c83f35a213f0eba845e5d635c15f
Vodovva <valerievodovnik@gmail.com>
2024-06-11 17:30:13 -0500

    lesson two completed

--- main.py
+++ main.py
@@ -18,4 +18,12 @@ print("Alternatively you can just \"escape\" the quote")
 
 print('Hello world!\nHello world!')
 
-print('Hello' + " "+ 'Valerie')
+print('Hello' + " " + 'Valerie')
+
+# Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+    
