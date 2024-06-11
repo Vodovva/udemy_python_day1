c9ba4dc32267670c0a0c0c82d5f415c767da5931
Vodovva <valerievodovnik@gmail.com>
2024-06-11 18:31:43 -0500

    lesson three completed

--- main.py
+++ main.py
@@ -27,3 +27,45 @@ print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
     
+
+print("What is your name?")
+
+print("Hello " + input("What is your name?"))
+
+# making notes
+
+#❌
+print(input("What is your name?"))
+#✅
+print(input())
+
+#✅
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+#❌
+num1 = int(input())
+num2 = int(input())
+
+print(6)
+
+# using len
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+# Write a program that calculates and outputs the number of characters in any name. The automated tests will try out lots of different names as the input. Your code should work for any name. Your code should only output the number, no other text is needed.
+# Hint
+# Remember, you can use len() around any piece of text to calculate the number of characters.
+# e.g. https://www.google.com/search?q=how+to+get+the+length+of+a+string+in+python+stack+overflow
+# Important
+# Don't add prompt text to the input() function.
+# e.g. use
+# ✅ name = input()
+# don't use:
+# ❌ name = input("What's your name?")
+
+num_of_letters = len(input())
+print(num_of_letters)
+
